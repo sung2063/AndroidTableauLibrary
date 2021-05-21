ea87d11560dad7d74511b02a03142efe46ff6091
Sung Hyun <sunghyunb1991@gmail.com>
2021-05-20 20:51:32 -0400

    Catch Exceptions On LinearProgressView and DotProgressView
    - Caught Exceptions On LinearProgressView and DotProgressView

--- TableauLibrary/src/main/java/com/sung2063/tableau_library/progress/LinearProgressView.kt
+++ TableauLibrary/src/main/java/com/sung2063/tableau_library/progress/LinearProgressView.kt
@@ -7,8 +7,10 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sung2063.tableau_library.R
+import com.sung2063.tableau_library.exception.IllegalArgumentException
 import com.sung2063.tableau_library.progress.adapter.LinearProgressItemsAdapter
 import com.sung2063.tableau_library.progress.handler.LinearProgressHandler
+import com.sung2063.tableau_library.util.RegexTool
 
 class LinearProgressView @JvmOverloads constructor(
     context: Context,
@@ -22,11 +24,30 @@ class LinearProgressView @JvmOverloads constructor(
     private var maxValue: Int
 
     init {
-        val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.LinearProgressView, 0, 0)
-        isUsingCommonColor = typedArray.getBoolean(R.styleable.LinearProgressView_useCommonColor, false)
+        val typedArray =
+            context.theme.obtainStyledAttributes(attrs, R.styleable.LinearProgressView, 0, 0)
+        isUsingCommonColor =
+            typedArray.getBoolean(R.styleable.LinearProgressView_useCommonColor, false)
         commonFilledColor = typedArray.getString(R.styleable.LinearProgressView_filledColor)
         commonUnfilledColor = typedArray.getString(R.styleable.LinearProgressView_unfilledColor)
         maxValue = typedArray.getInteger(R.styleable.LinearProgressView_maxValue, 100)
+
+        // Check conditions & exceptions
+        if (commonFilledColor == null) {
+            commonFilledColor = "#023047"      // Default color
+        } else if (!RegexTool.HEX_COLOR_CODE.toRegex()
+                .matches(input = commonFilledColor.toString())
+        ) {
+            throw IllegalArgumentException(context.getString(R.string.hex_color_code_illegal_error))
+        }
+
+        if (commonUnfilledColor == null) {
+            commonUnfilledColor = "#fbf8fa"      // Default color
+        } else if (!RegexTool.HEX_COLOR_CODE.toRegex()
+                .matches(input = commonUnfilledColor.toString())
+        ) {
+            throw IllegalArgumentException(context.getString(R.string.hex_color_code_illegal_error))
+        }
     }
 
     fun setHandler(_handler: LinearProgressHandler) {
@@ -35,8 +56,17 @@ class LinearProgressView @JvmOverloads constructor(
         val recyclerView: RecyclerView = findViewById(R.id.rv_linear_progress_list)
 
         // Create a RecyclerView with user data
-        val adapter = LinearProgressItemsAdapter(context, _handler.dataList, isUsingCommonColor, commonFilledColor, commonUnfilledColor, maxValue, _handler.space)
-        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        val adapter = LinearProgressItemsAdapter(
+            context,
+            _handler.dataList,
+            isUsingCommonColor,
+            commonFilledColor,
+            commonUnfilledColor,
+            maxValue,
+            _handler.space
+        )
+        recyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         recyclerView.adapter = adapter
     }
 }
